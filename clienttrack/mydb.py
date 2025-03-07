8abeb2331d2a8a6bbaa3086bfc52b98cbee65a72
Samarth Kaul <samarth.kaul@sonartechnologies.com>
2025-03-07 13:45:30 +0530

    couple of template pages added

--- clienttrack/mydb.py
+++ clienttrack/mydb.py
@@ -1,7 +1,6 @@
 # Install Mysql on your computer
 # https://dev.mysql.com/downloads/installer/
 # pip install mysql
-# pip install mysql-connector
 # pip install mysql-connector-python 
 
 import mysql.connector
