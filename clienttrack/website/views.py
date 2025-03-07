a02dabf85b67562a3e557257a363971bc5bd49a1
Samarth Kaul <samarth.kaul@sonartechnologies.com>
2025-03-07 14:57:36 +0530

    setting up views

--- clienttrack/website/views.py
+++ clienttrack/website/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-# from .forms import SignUpForm, AddRecordForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
